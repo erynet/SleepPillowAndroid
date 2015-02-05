3d2ea05870366298d0fa3ff8221f8a69588c3751
erynet <ldh668@gmail.com>
2015-02-05 22:43:43 +0900

    20150205

--- app/src/main/java/sleeprevolution/net/sleeppillow/LoadingActivity.java
+++ app/src/main/java/sleeprevolution/net/sleeppillow/LoadingActivity.java
@@ -1,39 +1,33 @@
 package sleeprevolution.net.sleeppillow;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class LoadingActivity extends ActionBarActivity {
-
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ */
+public class LoadingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        Loading();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_loading, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void Loading() {
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 1000);
     }
 }
